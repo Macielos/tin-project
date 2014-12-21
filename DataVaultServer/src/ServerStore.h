303438a2f747b7b9515e19b8a370ead41b1a6134
zabor <zabor@debian>
2014-12-21 14:13:42 +0100

    maly push, troche testujacy

--- DataVaultServer/src/ServerStore.h
+++ DataVaultServer/src/ServerStore.h
@@ -1,6 +1,15 @@
+#include <string>
+
 class ServerStore
 {
     public:
         ServerStore();
         ~ServerStore();
+        /*
+        String list(String username);
+        int save(String username, String filename);
+        int load(String username, String filename);
+        int remove(String username, String filename);
+        int rename(String username, String oldname, String newname);
+        */
 };
