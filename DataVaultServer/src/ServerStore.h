3fc32a6a14d6fec687c6dd7ca97874e0de66b19b
mateuszzaborski <matzaborski@gmail.com>
2015-01-05 13:12:50 +0100

    Update ServerStore.h

--- DataVaultServer/src/ServerStore.h
+++ DataVaultServer/src/ServerStore.h
@@ -1,3 +1,5 @@
+#include "User.h"
+#include <vector>
 #include <string>
 
 using namespace std;
@@ -7,10 +9,32 @@ class ServerStore
     public:
         ServerStore();
         ~ServerStore();
-
-        string list(string username);
-        int save(string username, string filename);
+        // zwraca "/nouser" jak nie ma usera
+        vector<string> list(string username);  // Co zwracać jak nie ma usera???
+        /*
+        zwraca 0 jak się udało
+        zwraca -1 jak brak usera
+        zwraca -2 jak plik o nazwie istnieje
+        */
+        int add(string username, string filename);
+        /*
+        zwraca 0 jak się udało
+        zwraca -1 jak brak usera
+        zwraca -2 jak plik o nazwie nie istnieje
+        */
         int remove(string username, string filename);
+        /*
+        zwraca 0 jak się udało
+        zwraca -1 jak brak usera
+        zwraca -2 jak plik o starej nazwie nie istnieje
+        zwraca -3 jak plik o nowej nazwie istnieje
+        */
         int rename(string username, string oldname, string newname);
+
+        // bedzie laczone pewnie z klasa michala
+        vector<User> users;
+
+    private:
+
 
 };
